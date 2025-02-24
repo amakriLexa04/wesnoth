960775e39006167fc90e78c6a88f648fd959bd5a
amakriLexa04 <ohrimale@gmail.com>
2025-02-24 13:55:07 +0200

    new wml tag, refresh_skills fix, start reworking spells in spellcasting.cfg

--- data/campaigns/The_Deceivers_Gambit/lua/spellcasting.lua
+++ data/campaigns/The_Deceivers_Gambit/lua/spellcasting.lua
@@ -400,7 +400,7 @@ end
 	 
 		wesnoth.game_events.fire(("refresh_" .. cfg.id .. "_skills"))
 		
-		for spell in wml.variables["caster_" .. cfg.id .. ".spell_unlocked"]:gmatch("[^,]+") do
+		for spell in wml.variables["caster_" .. cfg.id .. ".spell_equipped"]:gmatch("[^,]+") do
 	    	wml.variables[spell] = nil
         end
 		skills_equipped = nil
@@ -688,6 +688,38 @@ end
                             end
                             table.insert(spell_to_equip, spell)
                         end
+                    end
+                end
+            end
+            
+            wml.variables["caster_" .. u.id .. ".spell_equipped"] = table.concat(spell_to_equip, ",")
+            wml.fire("refresh_skills", { id = u.id })
+        end
+    end
+	
+	wml_actions["unequip_spell"] = function(cfg)
+        if not cfg.spell_id then return end
+        
+        local filter = wml.get_child(cfg, "filter") or wml.error "[unequip_spell] missing required [filter] tag"
+        local units = wesnoth.units.find(filter)
+        local spell_to_remove = {}
+        
+        for spell in cfg.spell_id:gmatch("[^,]+") do
+            table.insert(spell_to_remove, spell)
+        end
+        
+        for _, u in ipairs(units) do
+            local spell_to_equip = {}
+            local equipped_var = wml.variables["caster_" .. u.id .. ".spell_equipped"] or ""
+            
+            for spell in equipped_var:gmatch("[^,]+") do
+                table.insert(spell_to_equip, spell)
+            end
+            
+            for _, spell in ipairs(spell_to_remove) do
+                for i = #spell_to_equip, 1, -1 do
+                    if spell_to_equip[i] == spell then
+                        table.remove(spell_to_equip, i)
                     end
                 end
             end
